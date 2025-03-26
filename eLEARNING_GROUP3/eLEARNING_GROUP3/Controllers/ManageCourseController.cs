3a422337b3122fd6757ea72202fd21c152b63ef0
Sang <sangphamz1000@gamil.com>
2025-03-26 11:45:32 +0700

    [el_3_addcourse] Develop Manage Course

---
+++ eLEARNING_GROUP3/eLEARNING_GROUP3/Controllers/ManageCourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eLEARNING_GROUP3.Controllers
+{
+    public class ManageCourseController
+    {
+        _courseCategoriesService = courseCategoriesService;
+        }
+
+    public IActionResult Index()
+        public async Task<IActionResult> Index()
+    {
+        return View();
+        var listAllCourse = await _courseService.GetAllCourseAsync();
+        return View(listAllCourse.ToList());
+    }
+
+
+
+
+    public async Task<IActionResult> AddCourse()
+    {
+        var userIdString = HttpContext.Session.GetString("UserId");
+    }
+}
