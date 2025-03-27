820ca7748b433b3c87d62943a5ba9fa35caeb095
hungtabe <trandinhhung717@gmail.com>
2025-03-27 18:36:26 +0700

    [el_9] Update Controllers

---
+++ eLEARNING_GROUP3/Areas/Mentor/Controllers/QuestionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineLearning.Areas.Mentor.Controllers
+{
+    [Area("Mentor")]
+    public class QuestionController : Controller
+    {
+        public IActionResult QuestionsView()
+        {
+            return View();
+        }
+    }
+}
---
+++ eLEARNING_GROUP3/Areas/Mentor/Controllers/QuizController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineLearning.Models.DTOs;
+using OnlineLearning.Services.Implementations;
+using OnlineLearning.Services.Interfaces;
+
+namespace OnlineLearning.Areas.Mentor.Controllers
+{
+    [Area("Mentor")]
+    public class QuizController : Controller
+    {
+        private readonly IQuizService _quizService;
+
+        public QuizController(IQuizService quizService)
+        {
+            _quizService = quizService;
+        }
+
+        // Hiển thị form tạo quiz
+       
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CreateQuiz(QuizDTO quizDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create",quizDTO);
+            }
+
+            await _quizService.CreateQuizAsync(quizDTO);
+            return RedirectToAction("Index", "Home");
+            // Điều hướng về trang danh sách Quiz
+        }
+    }
+}
